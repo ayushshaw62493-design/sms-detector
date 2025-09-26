0ac2d1cb8fa9fdf867fdb4683934d8b87996e8e6
Ayush Shaw <ayushshaw62493@gmail.com>
2025-09-26 19:15:59 +0530

    Fixed NLTK LookupError, added local nltk_data

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import streamlit as st
 import pickle
 import string
@@ -5,41 +6,47 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 
-# Initialize PorterStemmer
+# ----------------------------
+# Use local nltk_data folder
+# ----------------------------
+nltk_data_path = os.path.join(os.path.dirname(__file__), "nltk_data")
+nltk.data.path.append(nltk_data_path)
+
 ps = PorterStemmer()
 
-# Download necessary NLTK data
-nltk.download('punkt', quiet=True)
-nltk.download('stopwords', quiet=True)
-
-from nltk.tokenize import word_tokenize
-
-
+# ----------------------------
+# Text preprocessing function
+# ----------------------------
 def transform_text(text):
-    # Lowercase the text
     text = text.lower()
+    text = nltk.word_tokenize(text)
 
-    # Tokenize text
-    tokens = word_tokenize(text)  # only 'punkt', no 'punkt_tab'
-
-    # Remove non-alphanumeric tokens
-    tokens = [word for word in tokens if word.isalnum()]
+    # Remove non-alphanumeric characters
+    y = [i for i in text if i.isalnum()]
+    text = y[:]
+    y.clear()
 
     # Remove stopwords and punctuation
-    tokens = [word for word in tokens if word not in stopwords.words('english') and word not in string.punctuation]
+    y = [i for i in text if i not in stopwords.words('english') and i not in string.punctuation]
+    text = y[:]
+    y.clear()
 
     # Apply stemming
-    tokens = [ps.stem(word) for word in tokens]
+    y = [ps.stem(i) for i in text]
 
-    return " ".join(tokens)
+    return " ".join(y)
 
+# ----------------------------
+# Load vectorizer and model
+# ----------------------------
+tfidf = pickle.load(open('vectorizer.pkl','rb'))
+model = pickle.load(open('model.pkl','rb'))
 
-# Load saved TF-IDF vectorizer and ML model
-tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
-model = pickle.load(open('model.pkl', 'rb'))
-
+# ----------------------------
 # Streamlit UI
+# ----------------------------
 st.title("Email/SMS Spam Classifier")
+
 input_sms = st.text_area("Enter the message")
 
 if st.button('Predict'):
@@ -52,7 +59,7 @@ if st.button('Predict'):
     # 3. Predict
     result = model.predict(vector_input)[0]
 
-    # 4. Display result
+    # 4. Display
     if result == 1:
         st.header("Spam")
     else:
